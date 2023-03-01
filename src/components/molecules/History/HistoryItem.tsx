bc0152ced99ae55c044f8a5671173916c69c9a32
Gian Franco <gianfraback@gmail.com>
2023-03-01 00:22:16 -0500

    feat history page as added

---
+++ src/components/molecules/History/HistoryItem.tsx
@@ -0,0 +1,29 @@
+import { ArrowIcon, DateIcon } from '@/components/atoms'
+import { ShoppingListItem } from '@/interfaces'
+import styles from './styles.module.css'
+
+type HistoryItemType = Omit<ShoppingListItem, 'id'>
+
+const HistoryItem = ({ name, status, createdAt }: HistoryItemType) => {
+  const date = new Date(createdAt).toLocaleDateString('en', { weekday: 'short', day: 'numeric', month: 'numeric', year: 'numeric' }).replace(/\//g, '.')
+
+  return (
+    <div className='bg-white shadow-md rounded-xl py-6 px-5 flex gap-3 lg:gap-0 flex-col lg:flex-row justify-between'>
+      <h2 className='font-quicksand font-medium text-base text-black'>
+        {name}
+      </h2>
+      <div className='flex gap-8 items-center'>
+        <span className='text-gray flex gap-3 font-quicksand text-xs font-medium items-center'>
+          <DateIcon />
+          {date}
+        </span>
+        <span className={`font-quicksand text-xs font-medium rounded-md py-1 px-2 ${status === 'completed' ? styles.completed : styles.incompleted}`}>
+          {status}
+        </span>
+        <ArrowIcon />
+      </div>
+    </div>
+  )
+}
+
+export default HistoryItem
