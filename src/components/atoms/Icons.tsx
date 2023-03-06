791bb1c9e375bd8e41b0c1ae199be3b49dda07a4
Gian Franco <gianfraback@gmail.com>
2023-03-06 00:13:38 -0500

    refactor product view and shoppinglist styles and logic was added

--- src/components/atoms/Icons.tsx
+++ src/components/atoms/Icons.tsx
@@ -73,29 +73,35 @@ export function CartIcon() {
   )
 }
 
-export function PlusIcon() {
+export interface PlusIconProps {
+  isShoppinglist: boolean
+}
+
+export function PlusIcon({ isShoppinglist }: PlusIconProps) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       height='24px'
       viewBox='0 0 24 24'
       width='24px'
-      fill='#C1C1C4'
+      fill={`${isShoppinglist ? '#F9A109' : '#C1C1C4'}`}
     >
       <path d='M0 0h24v24H0V0z' fill='none' />
       <path d='M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z' />
     </svg>
   )
 }
-
-export function PencilIcon() {
+export interface PencilIconProps {
+  status: boolean
+}
+export function PencilIcon({ status }: PencilIconProps) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
       height='24px'
       viewBox='0 0 24 24'
       width='24px'
-      fill='#000000'
+      fill={`${status ? '#000000' : '#F9A109'}`}
     >
       <path d='M0 0h24v24H0z' fill='none' />
       <path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z' />
@@ -110,7 +116,7 @@ export function MinusIcon() {
       height='24px'
       viewBox='0 0 24 24'
       width='24px'
-      fill='#000000'
+      fill='#F9A109'
     >
       <path d='M0 0h24v24H0z' fill='none' />
       <path d='M19 13H5v-2h14v2z' />
@@ -125,7 +131,7 @@ export function DeleteIcon() {
       height='24px'
       viewBox='0 0 24 24'
       width='24px'
-      fill='#000000'
+      fill='#fff'
     >
       <path d='M0 0h24v24H0z' fill='none' />
       <path d='M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z' />
