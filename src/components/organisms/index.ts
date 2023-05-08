f5d1edd1b09ebd42c52b203272440ec3501042c1
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-08 18:41:51 -0500

    feat: remove files and pages index

--- src/components/organisms/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './AddNewItem'
-export * from './Categories'
-export { default as NoSSRWrapper } from './NoSSRWrapper'
-export * from './ShoppingList'
-export * from './ProductDetails'
