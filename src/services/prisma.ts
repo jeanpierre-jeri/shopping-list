2a2ac547ebdc6afbc35b15a0147c35a48a61217c
Jean Pierre <jeanpierre.webdev@gmail.com>
2023-05-08 18:42:09 -0500

    refactor: fix prisma in development

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,5 +1,12 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient()
+declare global {
+  // eslint-disable-next-line vars-on-top, no-var
+  var prisma: PrismaClient | undefined
+}
+
+const prisma = globalThis.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
 
 export { prisma }
